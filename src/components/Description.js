e53316280195639f28627a51bd60e39ee2193660
Fabio Cortés <fcor14@hotmail.com>
2018-04-19 18:20:59 -0500

    more icons and texts added

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Description = () =>
   <section className="description column">
     <h1 className="description-title">
-      Topping marzipan brownie tootsie roll cake cupcake.
+      Optimiza el valor de tu cadena de suministro
     </h1>
     <p className=" text description-text">
       In just two hours, capture the critical human insights you need to conﬁdently deliver what
--- src/components/Storage.js
+++ src/components/Storage.js
@@ -1,24 +1,27 @@
 import React from 'react'
+import city from '../images/city.png'
+import frontera from '../images/frontera.png'
+import puerto from '../images/puerto.png'
 
 const Storage = () =>
   <section className="storage column">
     <h1 className="storage-title section-title">
-      Jelly dessert marshmallow jelly
+      Contamos con servicios de almacenaje en sitios estratégicos
     </h1>
     <p className="text storage-text">
       Powder lollipop I love. Jelly dessert marshmallow jelly. Croissant dragée cookie. Pudding candy jelly beans cotton candy tootsie roll. Bonbon
       chocolate topping lemon drops oat cake. Tart gummi bears toee. Gingerbread sweet roll candy chocolate marshmallow I love I love topping
     </p>
     <div className="storage-cards row">
-      <Card />
-      <Card />
-      <Card />
+      <Card icon={city}/>
+      <Card icon={frontera}/>
+      <Card icon={puerto}/>
     </div>
   </section>
 
-const Card = () =>
+const Card = (props) =>
   <div className="s-card column">
-    <div className="icon"></div>
+    <img className="t-icon" src={props.icon} alt="icon" />
     <p className="text">
       Powder lollipop I love. Jelly dessert
       marshmallow jelly. Croissant dragée
